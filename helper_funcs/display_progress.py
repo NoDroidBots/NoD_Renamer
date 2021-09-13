192088086ecc6a576a8c7b3e01a2413af182d35d
NoDroidBots <90566330+NoDroidBots@users.noreply.github.com>
2021-09-14 00:27:37 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,7 +42,7 @@ async def progress_for_pyrogram(
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>Completed:</b>{0} \n<b>💾 Total Size:</b> {1}\n<b>🚀 Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
+        tmp = progress + "<b>✅ Completed:</b>{0} \n<b>💾 Total Size:</b> {1}\n<b>🚀 Speed:</b> {2}/s\n<b>⌚️ ETA:</b> {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
